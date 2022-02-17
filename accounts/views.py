ba1032de14433a50a3f23ffa675429740ca871d1
evesquare <jonjonjonjon152@gmail.com>
2022-02-17 16:04:47 +0900

    [add] ユーザ認証api実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from accounts.serializers import UserSerializer
+
+User = get_user_model()
+
+class UserList(ListAPIView):
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserSerializer
