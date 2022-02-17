4091f76ecc4dbb9355afcf9d60f21b2483c6bd15
evesquare <jonjonjonjon152@gmail.com>
2022-02-17 23:06:26 +0900

    [add] playlist

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,21 @@
+from enum import unique
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    
+    email = models.EmailField(
+        "メールアドレス",
+        max_length=255,
+        unique=True,
+    )
+
+    name = models.CharField(
+        "ユーザネーム",
+        max_length=255,
+    )
+
+    def __str__(self):
+        return self.name
