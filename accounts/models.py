e7b2f052ac728e87527d2e36e90a598fc0f9cc20
evesquare <jonjonjonjon152@gmail.com>
2022-02-18 21:52:35 +0900

    [add] accountsとplaylistのmodelを定義

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,78 @@
-from enum import unique
-
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from playlist.models import PlayList
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Meta:
+        verbose_name = _("ユーザ")
+        verbose_name_plural = _("ユーザ一覧")
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    username_validator = UnicodeUsernameValidator()
     
+    username = models.CharField(
+        _("ユーザネーム"),
+        max_length=255,
+        validators=[username_validator],
+        blank=False,
+    )
+
     email = models.EmailField(
-        "メールアドレス",
+        _("メールアドレス"),
         max_length=255,
         unique=True,
     )
 
-    name = models.CharField(
-        "ユーザネーム",
-        max_length=255,
+    is_staff = models.BooleanField(_("staff status"), default=False)
+    is_active = models.BooleanField(_("active"), default=True)
+
+    objects = UserManager()
+
+    playlists = models.ForeignKey(
+        PlayList,
+        verbose_name="プレイリスト",
+        related_name="user_playlists",
+        on_delete=models.CASCADE,
+        null=True,
     )
 
     def __str__(self):
-        return self.name
+        return self.username
+
+
+class Follow(models.Model):
+
+    class Meta:
+        verbose_name = _("フォロー")
+        verbose_name_plural = _("フォロー一覧")
+
+    user = models.ForeignKey(
+        User,
+        verbose_name="ユーザ",
+        related_name="follow",
+        on_delete=models.CASCADE,
+        blank=False,
+    )
+
+    target = models.ForeignKey(
+        User,
+        verbose_name="フォローユーザ",
+        related_name="target_user",
+        on_delete=models.CASCADE,
+        blank=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.user.username}"
--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Content(models.Model):
+
+    class Meta:
+        verbose_name = _("保存コンテンツ")
+        verbose_name_plural = _("保存コンテンツ一覧")
+
+    name = models.CharField(
+        verbose_name="コンテンツ名",
+        max_length=510,
+        blank=False,
+    )
+
+    url = models.URLField(
+        verbose_name="コンテンツURL",
+        max_length=510,
+        blank=False,
+    )
+
+    thumbnail = models.URLField(
+        verbose_name="サムネイルURL",
+        max_length=510,
+        null=True,
+    )
+
+    CONTENT_TYPES = (
+        ("nikodo", "ニコニコ動画"),
+        ("youtube", "Youtube"),
+        ("spotify", "Spotify"),
+    )
+
+    content_type = models.CharField(
+        verbose_name="コンテンツのタイプ",
+        choices=CONTENT_TYPES,
+        max_length=50,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content_type} {self.name}"
+
+class PlayList(models.Model):
+
+    class Meta:
+        verbose_name = _("プレイリスト")
+        verbose_name_plural = _("プレイリスト一覧")
+
+    name = models.CharField(
+        verbose_name="プレイリスト名",
+        max_length=510,
+        blank=False,
+        unique=True,
+    )
+
+    contents = models.ManyToManyField(
+        Content,
+        verbose_name="コンテンツ",
+        related_name="contents",
+    )
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.contents.count()})"
