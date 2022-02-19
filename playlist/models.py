79cff55c7c6fb5bc935a0c408ba7ee35a8742269
evesquare <jonjonjonjon152@gmail.com>
2022-02-19 14:09:49 +0900

    [move] playlistのモデルを移動

--- playlist/models.py
+++ playlist/models.py
@@ -1,5 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+class PlayList(models.Model):
+
+    class Meta:
+        verbose_name = _("プレイリスト")
+        verbose_name_plural = _("プレイリスト一覧")
+
+    user = models.ForeignKey(
+        User,
+        verbose_name=_("ユーザ"),
+        related_name="user",
+        on_delete=models.CASCADE,
+    )
+
+    name = models.CharField(
+        verbose_name="プレイリスト名",
+        max_length=510,
+        blank=False,
+        unique=True,
+    )
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Content(models.Model):
@@ -7,10 +37,23 @@ class Content(models.Model):
     class Meta:
         verbose_name = _("保存コンテンツ")
         verbose_name_plural = _("保存コンテンツ一覧")
+        unique_together = ["playlist", "order"]
+        ordering = ['order']
+
+    playlist = models.ForeignKey(
+        PlayList,
+        verbose_name=_("プレイリスト"),
+        related_name="contents",
+        on_delete=models.CASCADE,
+    )
+
+    order = models.IntegerField(
+        verbose_name=_("並び順"),
+    )
 
     name = models.CharField(
         verbose_name="コンテンツ名",
-        max_length=510,
+        max_length=255,
         blank=False,
     )
 
@@ -41,30 +84,4 @@ class Content(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.content_type} {self.name}"
-
-class PlayList(models.Model):
-
-    class Meta:
-        verbose_name = _("プレイリスト")
-        verbose_name_plural = _("プレイリスト一覧")
-
-    name = models.CharField(
-        verbose_name="プレイリスト名",
-        max_length=510,
-        blank=False,
-        unique=True,
-    )
-
-    contents = models.ManyToManyField(
-        Content,
-        verbose_name="コンテンツ",
-        related_name="contents",
-    )
-
-    updated_at = models.DateTimeField(auto_now=True)
-
-    create_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.name} ({self.contents.count()})"
+        return f"{self.content_type} {self.name} ({self.order})"
