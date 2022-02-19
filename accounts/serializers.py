057c2aa2331ccc5b3d768fa684dca1aecc55c8f9
evesquare <jonjonjonjon152@gmail.com>
2022-02-19 14:18:05 +0900

    [add] accountsのエンドポイントを作成

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from accounts.models import Follow
+
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,4 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
             "id",
             "username",
             "email",
+        )
+
+
+class FollowSerializer(serializers.HyperlinkedModelSerializer):
+
+    
+    
+    class Meta:
+        model = Follow
+        fields = (
+            "user",
+            "target",
+            "created_at",
         )
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from accounts.views import UserList
+from accounts.views import FollowDetailViewSet, UserDetail, Users
+
+view_name = "accounts"
 
 urlpatterns = [
-    path("users/", UserList.as_view()),
+    path("users/", Users.as_view(), name="users"),
+    path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
+    path("follow/<int:pk>/", FollowDetailViewSet.as_view(), name="follow_detail"),
 ]
