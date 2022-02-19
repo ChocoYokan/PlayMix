085eb104c6b9232e729ed6dc30acfe88ab615b4c
evesquare <jonjonjonjon152@gmail.com>
2022-02-19 14:18:05 +0900

    [add] accountsのエンドポイントを作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,32 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
+from rest_framework import generics, permissions, viewsets
+from rest_framework.generics import CreateAPIView, ListAPIView
 
-from accounts.serializers import UserSerializer
+from accounts.models import Follow
+from accounts.serializers import FollowSerializer, UserSerializer
 
 User = get_user_model()
 
-class UserList(ListAPIView):
+class Users(CreateAPIView):
+    """
+    ユーザの作成ができるエンドポイント
+    """
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    ユーザの編集・表示ができるエンドポイント
+    """
+
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserSerializer
+        
+class FollowDetailViewSet(ListAPIView):
+    """
+    フォローの編集・表示ができるエンドポイント
+    """
+
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permissions_class = [None]
