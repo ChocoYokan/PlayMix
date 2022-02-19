84deda8720db8cfcb0acd39a1964d5269c8d96ee
evesquare <jonjonjonjon152@gmail.com>
2022-02-19 14:35:15 +0900

    [fix] typo

--- playlist/models.py
+++ playlist/models.py
@@ -26,7 +26,7 @@ class PlayList(models.Model):
 
     updated_at = models.DateTimeField(auto_now=True)
 
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
