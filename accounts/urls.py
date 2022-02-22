f8899a4e7de53f7279a5b233daea6b3cee1b7e04
evesquare <jonjonjonjon152@gmail.com>
2022-02-23 00:41:50 +0900

    [update] urlpatternsを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
 from rest_framework import routers
+
 from accounts.views import FollowDetailViewSet, FollowerDetailViewSet
 
 view_name = "accounts"
@@ -7,3 +7,4 @@ view_name = "accounts"
 router = routers.DefaultRouter()
 router.register(r"follow", FollowDetailViewSet)
 router.register(r"follower", FollowerDetailViewSet)
+urlpatterns = router.urls
